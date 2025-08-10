8fe1ae3e4c5565ff3a102dcc0cd5653ab6887fda
Hussien Ahmed <hussienahmed1122006@gmail.com>
2025-08-10 08:28:24 +0300

    Adding View Query

--- Project.Infrastructure/ViewEmployeeData/Persistence/ViewEmployeeDataConfiguration.cs
+++ Project.Infrastructure/ViewEmployeeData/Persistence/ViewEmployeeDataConfiguration.cs
@@ -9,6 +9,22 @@ namespace Project.Infrastructure.ViewEmployeeData.Persistence
         {
             builder.HasNoKey()
                 .ToView("ViewEmployeeData");
+
+            /*
+             
+            create view ViewEmployeeData as
+            select e.* , f.Path, T.*,R.RegionDescription
+            from Employees e
+            left join FilePaths f
+            on e.EmployeeGuid = f.EntityGuid
+            left join EmployeeTerritories ET
+            on e.EmployeeID = ET.EmployeeID
+            left join Territorie T
+            on ET.TerritoryID = T.TerritoryID
+            left join Regions R
+            on R.RegionID = T.RegionID
+             
+         */
         }
     }
 }
