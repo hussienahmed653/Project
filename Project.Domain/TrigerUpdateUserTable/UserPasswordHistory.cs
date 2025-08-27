0fc21867e7ff9b10517108462cfc212a496b6a52
hussienahmed1122006 <hussienahmed1122006@gmail.com>
2025-08-27 22:50:18 +0300

    finalcommit

--- Project.Domain/TrigerUpdateUserTable/UserPasswordHistory.cs
+++ Project.Domain/TrigerUpdateUserTable/UserPasswordHistory.cs
@@ -4,5 +4,21 @@
     {
         public Guid UserGuid { get; set; }
         public string OldPassword { get; set; }
+
+        /*
+         
+            create trigger user_Password_Histories
+            on Users
+            after update
+            as
+            begin
+	            if UPDATE(PasswordHash)
+	            begin
+		            insert into userPasswordHistories(UserGuid,OldPassword)
+		            select d.Id, d.PasswordHash from deleted d
+	            end
+            end
+         
+         */
     }
 }
